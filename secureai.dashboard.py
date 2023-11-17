bbac2ff28d727405e3e190c6e3389c7b26e396e8
Samuel Shamber <sshamber@secureai.dev>
2023-11-17 11:42:05 -0500

    test

--- secureai.dashboard.py
+++ secureai.dashboard.py
@@ -4,12 +4,11 @@ import pandas as pd
 import plotly.express as px
 import numpy as np
 
-# Sample data generation (Replace with real threat data in actual implementation)
+# Sample data generation
 def generate_sample_data():
     dates = pd.date_range(start='2023-01-01', end='2023-01-31', freq='H')
     categories = ['Malware', 'Phishing', 'DDoS', 'Insider Threat']
     severity_levels = ['High', 'Medium', 'Low']
-
     data = pd.DataFrame({
         'Date': np.random.choice(dates, size=100),
         'Category': np.random.choice(categories, size=100),
@@ -17,13 +16,9 @@ def generate_sample_data():
     })
     return data
 
-# In-memory storage for incident reports
-if 'incident_reports' not in st.session_state:
-    st.session_state['incident_reports'] = pd.DataFrame(columns=['Date', 'Category', 'Severity', 'Description'])
-
-# Authentication (placeholder, implement real authentication for production)
+# Authentication
 def authenticate_user(username, password):
-    return username == "admin" and password == "password"  # Replace with real authentication
+    return username == "admin" and password == "password"  # Placeholder
 
 # Calculate threat level
 def calculate_threat_level(data):
@@ -40,13 +35,12 @@ def calculate_threat_level(data):
     else:
         return 'Yellow', high_severity_count, total_count
 
-# Function to convert DataFrame to CSV for download
+# Function to convert DataFrame to CSV
 def convert_df_to_csv(df):
     return df.to_csv(index=False).encode('utf-8')
 
-# Sidebar with login
+# Sidebar for Login and Forms
 with st.sidebar:
-    # Login Section
     with st.expander("Login", expanded=True):
         username = st.text_input("Username", key="login_username")
         password = st.text_input("Password", type="password", key="login_password")
@@ -56,26 +50,42 @@ with st.sidebar:
         else:
             st.session_state['authenticated'] = False
 
+    with st.expander("Filters", expanded=True):
+        selected_severity = st.multiselect('Select Severity Level', ['High', 'Medium', 'Low'], key="filter_severity")
+        selected_category = st.multiselect('Select Threat Category', ['Malware', 'Phishing', 'DDoS', 'Insider Threat'], key="filter_category")
+
+    with st.expander("Incident Reporting", expanded=False):
+        with st.form(key="incident_reporting_form"):
+            form_date = st.date_input("Date", key="form_date")
+            form_category = st.selectbox("Category", ['Malware', 'Phishing', 'DDoS', 'Insider Threat'], key="form_category")
+            form_severity = st.selectbox("Severity", ['High', 'Medium', 'Low'], key="form_severity")
+            form_description = st.text_area("Description", key="form_description")
+            submit_button = st.form_submit_button("Report Incident")
+
+            # Add new incident to the DataFrame
+            if submit_button:
+                new_incident = pd.DataFrame([[form_date, form_category, form_severity, form_description]],
+                                            columns=['Date', 'Category', 'Severity', 'Description'])
+                st.session_state['incident_reports'] = pd.concat([st.session_state['incident_reports'], new_incident], ignore_index=True)
+
+# Check if user is authenticated
 if st.session_state.get('authenticated', False):
     st.title('Threat Intelligence Dashboard')
 
-    # Main dashboard layout with left-aligned arrangement
     # Row 1: Threat Level and Real-time Monitoring Chart
-    row1_col1, row1_col2, row1_col3 = st.columns([2, 3, 1])
+    row1_col1, row1_col2 = st.columns([2, 5])
     with row1_col1:
-        # Threat Level Display
         data = generate_sample_data()
         threat_level, high_severity_count, total_count = calculate_threat_level(data)
         st.markdown(f"## Threat Level: {threat_level}")
         st.markdown(f"### Details: {high_severity_count} high severity threats out of {total_count} total threats.")
 
     with row1_col2:
-        # Real-time Monitoring Chart
         fig = px.histogram(data, x='Date', y='Category', color='Severity', barmode='group')
         st.plotly_chart(fig)
 
     # Row 2: Data Table and Incident Reports
-    row2_col1, row2_col2, row2_col3 = st.columns([2, 2, 1])
+    row2_col1, row2_col2 = st.columns([2, 5])
     with row2_col1:
         st.write("Real-time Threat Monitoring")
         st.dataframe(data)
@@ -84,20 +94,18 @@ if st.session_state.get('authenticated', False):
         st.write("Incident Reports")
         st.dataframe(st.session_state['incident_reports'])
 
-    # Row 3: Download Data Button
-    row3_col1, row3_col2 = st.columns([1, 4])
-    with row3_col1:
-        st.download_button(
-            label="Download data as CSV",
-            data=convert_df_to_csv(data),
-            file_name='threat_data.csv',
-            mime='text/csv',
-        )
+    # Download Button
+    st.download_button(
+        label="Download data as CSV",
+        data=convert_df_to_csv(data),
+        file_name='threat_data.csv',
+        mime='text/csv',
+    )
 
-    # Footer
     st.write("SecureAI Threat Intelligence Dashboard")
 else:
     st.info("Please log in to access the dashboard.")
+
 
 
 
